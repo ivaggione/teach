5158db70fc30dd942938003112e5258005f57578
Ignacio Vaggione <nass-mac@NaSs-MacBook-Pro.local>
2014-05-30 16:18:53 -0300

    Added users to installed apps in settings. Added fields to user profile

--- users/models.py
+++ users/models.py
@@ -250,6 +250,40 @@ COUNTRY_CHOICES = (
     ('ZWE', _('Zimbabwe')),
 )
 
+TIMEZONE_CHOICES = (
+    ('-12.0', '(GMT -12:00) Eniwetok, Kwajalein'),
+    ('-11.0', '(GMT -11:00) Midway Island, Samoa'),
+    ('-10.0', '(GMT -10:00) Hawaii'),
+    ('-9.0', '(GMT -9:00) Alaska'),
+    ('-8.0', '(GMT -8:00) Pacific Time (US &amp; Canada)'),
+    ('-7.0', '(GMT -7:00) Mountain Time (US &amp; Canada)'),
+    ('-6.0', '(GMT -6:00) Central Time (US &amp; Canada), Mexico City'),
+    ('-5.0', '(GMT -5:00) Eastern Time (US &amp; Canada), Bogota, Lima'),
+    ('-4.0', '(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz'),
+    ('-3.5', '(GMT -3:30) Newfoundland'),
+    ('-3.0', '(GMT -3:00) Brazil, Buenos Aires, Georgetown'),
+    ('-2.0', '(GMT -2:00) Mid-Atlantic'),
+    ('-1.0', '(GMT -1:00 hour) Azores, Cape Verde Islands'),
+    ('0.0', '(GMT) Western Europe Time, London, Lisbon, Casablanca'),
+    ('1.0', '(GMT +1:00 hour) Brussels, Copenhagen, Madrid, Paris'),
+    ('2.0', '(GMT +2:00) Kaliningrad, South Africa'),
+    ('3.0', '(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg'),
+    ('3.5', '(GMT +3:30) Tehran'),
+    ('4.0', '(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi'),
+    ('4.5', '(GMT +4:30) Kabul'),
+    ('5.0', '(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent'),
+    ('5.5', '(GMT +5:30) Bombay, Calcutta, Madras, New Delhi'),
+    ('5.75', '(GMT +5:45) Kathmandu'),
+    ('6.0', '(GMT +6:00) Almaty, Dhaka, Colombo'),
+    ('7.0', '(GMT +7:00) Bangkok, Hanoi, Jakarta'),
+    ('8.0', '(GMT +8:00) Beijing, Perth, Singapore, Hong Kong'),
+    ('9.0', '(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk'),
+    ('9.5', '(GMT +9:30) Adelaide, Darwin'),
+    ('10.0', '(GMT +10:00) Eastern Australia, Guam, Vladivostok'),
+    ('11.0', '(GMT +11:00) Magadan, Solomon Islands, New Caledonia'),
+    ('12.0', '(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka')
+)
+
 
 class UserProfile(models.Model):
 
@@ -262,11 +296,15 @@ class UserProfile(models.Model):
     gender = models.CharField(null=True, blank=True, max_length=3, choices=GENDER_CHOICES)
     birthDate = models.DateField(null=True, blank=True)
     jobTitle = models.CharField(null=True, blank=True, max_length=50)
-    nationality = models.CharField(null=True, blank=True, max_length=3, choices=COUNTRY_CHOICES)
+    nationality = models.CharField(null=True, blank=True, max_length=50)
     alumniOf = models.CharField(null=True, blank=True, max_length=50)
     interests = models.TextField(null=True, blank=True)
     isUpgraded = models.BooleanField(default=False)
     youtubeChannel = models.URLField(null=True, blank=True)
+    timezone = models.CharField(null=True, blank=True, choices=TIMEZONE_CHOICES)
+
+    def __unicode__(self):
+        return self.user.email + u" Profile"
 
 
 def create_profile(sender, instance, **kwargs):
